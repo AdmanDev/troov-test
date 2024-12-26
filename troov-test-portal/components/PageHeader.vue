552c26ac5c4d234177f3ea6702260c361f432390
ADMAN <adman.software@gmail.com>
2024-12-26 17:44:58 +0100

    feat: add page header

---
+++ troov-test-portal/components/PageHeader.vue
@@ -0,0 +1,20 @@
+<template>
+  <nav class="navbar navbar-expand-lg navbar-light bg-white shadow-sm">
+    <div class="container">
+      <NuxtLink
+        class="navbar-brand d-flex align-items-center"
+        to="/"
+      >
+        <i class="bi bi-box me-2 text-primary" />
+        <span class="fw-bold">Troov</span>
+      </NuxtLink>
+
+      <button
+        class="btn btn-primary"
+      >
+        <i class="bi bi-plus-lg me-2" />
+        Signaler un objet perdu
+      </button>
+    </div>
+  </nav>
+</template>
